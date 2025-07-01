7c18100795559e46ef0e49e559d37ab53d6c4be0
SuhaibBaba <suhaib.baba@xngage.com>
2025-07-02 01:45:04 +0300

    Add animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@public/styles/style.scss";
 import Hero from "@app/widgets/Hero";
@@ -8,8 +10,19 @@ import Experience from "@app/widgets/Experience";
 import Testimonial from "@app/widgets/Testimonial";
 import Contact from "@app/widgets/Contact";
 import homePageData from "../data/HomePagdData.json";
+import "aos/dist/aos.css";
+import Aos from "aos";
+import "slick-carousel/slick/slick.css";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    Aos.init({
+      once: true,
+    });
+  }, []);
+
   return (
     <div className="">
       <Hero {...homePageData.hero} socialLinks={homePageData.socialLinks} />
--- src/app/widgets/Header.tsx
+++ src/app/widgets/Header.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState, useEffect } from "react";
 import { Link as ScrollLink } from "react-scroll";
 import Link from "next/link";
