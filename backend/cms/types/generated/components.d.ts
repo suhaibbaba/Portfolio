83a9d186371dc3287c2468779d3f63f322f44f16
SuhaibBaba <suhaib.baba@xngage.com>
2025-07-12 23:52:17 +0300

    create schema for strapi

---
+++ backend/cms/types/generated/components.d.ts
@@ -0,0 +1,44 @@
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface LayoutFooter extends Struct.ComponentSchema {
+  collectionName: 'components_layout_footers';
+  info: {
+    displayName: 'Footer';
+  };
+  attributes: {
+    message: Schema.Attribute.Blocks;
+  };
+}
+
+export interface LayoutHeader extends Struct.ComponentSchema {
+  collectionName: 'components_layout_headers';
+  info: {
+    displayName: 'Header';
+  };
+  attributes: {
+    contactCta: Schema.Attribute.Component<'shared.link', false>;
+    logo: Schema.Attribute.Media<'images'>;
+    navItems: Schema.Attribute.Component<'shared.link', true>;
+  };
+}
+
+export interface SharedLink extends Struct.ComponentSchema {
+  collectionName: 'components_shared_links';
+  info: {
+    displayName: 'Link';
+  };
+  attributes: {
+    href: Schema.Attribute.String & Schema.Attribute.Required;
+    label: Schema.Attribute.String;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'layout.footer': LayoutFooter;
+      'layout.header': LayoutHeader;
+      'shared.link': SharedLink;
+    }
+  }
+}
